443cd5001ac577fda7bd809b1c7fcda9e9232f87
ohju96 <ohju96@naver.com>
2022-08-26 21:07:55 +0900

    책의 분야 추가

--- library-app/src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -9,12 +9,28 @@ import javax.persistence.Id
 class Book(
         val name: String,
 
+        val type : String,
+
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long? = null
 ) {
     init {
         if (name.isBlank()) {
             throw IllegalArgumentException("이름은 비어있을 수 없습니다.")
+        }
+    }
+
+    companion object {
+        fun fixture(
+                name: String = "책 이름",
+                type: String = "COMPUTER",
+                id: Long? = null,
+        ): Book {
+            return Book(
+            name = name,
+            type = type,
+            id = id,
+            )
         }
     }
 }
