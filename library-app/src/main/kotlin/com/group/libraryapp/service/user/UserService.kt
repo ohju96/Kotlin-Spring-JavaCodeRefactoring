1f0c6e9d670b1afca447b90f32ee9f89b8b98fbd
ohju96 <ohju96@naver.com>
2022-08-27 15:21:36 +0900

    유저 대출 현황

--- library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -2,12 +2,14 @@ package com.group.libraryapp.service.user
 
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.BookHistoryResponse
+import com.group.libraryapp.dto.user.response.UserLoanHistoryResponse
 import com.group.libraryapp.dto.user.response.UserResponse
 import com.group.libraryapp.util.fail
 import com.group.libraryapp.util.findByIdOrThrow
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -42,5 +44,22 @@ class UserService(
 //        val user = userRepository.findByName(name) ?: throw IllegalArgumentException() //옵셔널을 사용한 것 처럼 사용
         val user = userRepository.findByName(name) ?: fail() //코틀린 파일에 예외처리 메서드를 추가해 처리한다.
         userRepository.delete(user)
+    }
+
+    @Transactional(readOnly = true)
+    fun getUserLoanHistories(): List<UserLoanHistoryResponse> {
+        return userRepository.findAll().map { user ->
+            UserLoanHistoryResponse(
+                name = user.name,
+                books = user.userLoanHistory.map { history ->
+                    BookHistoryResponse(
+                            name = history.bookName,
+
+                            isReturn = history.status == UserLoanStatus.RETURNED
+
+                    )
+                }
+            )
+        }
     }
 }
